85ea1aa3ca7d24fcef584225eadfe668f74add78
jovanidesouza <jovanidesouza@hotmail.com>
2018-07-07 23:18:43 -0300

    Att
    Finalizado, faltando detecção de queda nos enlaces.

--- T2 - Roteamento com UDP e Bellman ford - Distribuído/roteador.c
+++ T2 - Roteamento com UDP e Bellman ford - Distribuído/roteador.c
@@ -5,12 +5,27 @@
 //Jovani de Souza, Davi Pegoraro
 
 #include "roteador.h"
-int ID, sock;
-mensagem temp_msg;
-roteador roteadores[NROTEADORES];//vetor de roteadores, usa paenas o ID, mas instancia um vetor para facilitar a leitura do arquivo
-struct sockaddr_in si_me, si_other;
-pthread_t recebe_id;
+int ID, op, x, sock;                                   
+int tabelaroteamento[NROTEADORES][NROTEADORES];
 
+                                  
+int vetordistancia[NROTEADORES];
+int vetorvizinhos[NROTEADORES];
+
+
+mensagem temp_msg;
+mensagem caixa_msg[100];
+int msg_control=0;
+mensagem caixa_env[100];
+int msg_env_control=0;
+mensagem temp_rota;//usada para enviar o vetordist para os vizinhos
+
+roteador roteadores[NROTEADORES];//vetor de roteadores, usa paenas o ID, mas instancia um vetor para facilitar a leitura do arquivo
+
+
+struct sockaddr_in si_me, si_other;
+pthread_t recebe_id, envia_id;
+pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
 
 
 int main(int argc, char const *argv[]) {
@@ -34,10 +49,227 @@ int main(int argc, char const *argv[]) {
 
   criar_roteador(ID);//carrega insformações do arquivo,(id, porta ,ip) cria socket e faz bind do socket com a porta
   pthread_create(&recebe_id, NULL, recebe, NULL); //Cria a thread receptora
-  sleep(5);
+  pthread_create(&envia_id, NULL, envia_vetor, NULL);//Cria a thread que envia
+  sleep(1);
+
+  criavetor();// cria o vetor distancia com os dados do arquivo enlaces.config
+  criatabela();//instancia a tabela com valores 0 na digonal e 1000 no resto
+  atualiza_vetor_vizinhos();
+  
+  //////////////////////
+
+  while(1){//loop menu, usado pra mandar e ler mensagens
+   
+
+    system("clear");
+      if(op == -1){
+        printf("ROTEADOR %d\n", ID);
+        printf("---------------------------------------------\n");
+        printf("0 - Atualizar\n");
+        printf("1 - Checar Mensagens\n");
+        printf("2 - Enviar Mensagem\n");
+        printf("3 - Sair\n");
+        printf("4 - Imprimir tabela\n\n");
+        printf("---------------------------------------------\n");
+        scanf("%d", &op);
+      }else if(op==0){
+        op=-1;
+      }else if(op==1){
+      
+        for(x=0; x<=msg_control; x++){
+          if(x<msg_control){
+            printf("Mensagem recebida de %d\n", caixa_msg[x].origem);
+          }
+          printf("%s\n", caixa_msg[x].msg);
+        }
+        sleep(10);
+        op=-1;
+      }else if(op==2){
+
+        envia(ID);
+        
+      }else if(op==3){
+        exit(1);
+
+      }else if(op==4){
+        imprime_tabela_roteamento();
+      }else{
+        op=-1;
+      }
+
+
+  }
 
 
   return 0;
+}
+
+
+
+void envia(){//envia mensagens, faz busca e fornece rota para o pacote
+
+  
+
+  int i=1,dest,next;
+  
+  printf("Digite o roteador destino\n");
+  scanf("%d", &dest);//destino da mensagem
+
+  if(dest>5 || dest<0){
+    die("Esse roteador nao existe\n");//trata erro na inserção do destino
+    op=-1;
+  }
+  
+
+  printf("Escreva a mensagem que quer enviar para %d\n", dest);
+  getchar();
+  fgets(caixa_env[msg_env_control].msg, MENSAGEM_SIZE, stdin);//mensagem no vetor de msgs_env// pega do teclado e guarda a msg na caixa 
+  //caixa_env[msg_control].num=msg_control;//seta id da mensagem
+  caixa_env[msg_env_control].origem= ID;
+  caixa_env[msg_env_control].destino= dest;
+  caixa_env[msg_env_control].tipo=0;
+
+  //até aqui// pega destino, pega msg 
+
+ //inserir função de roteamento aqui, que gera o proximo a ser roteado  
+
+  printf("enviando para %d atraves de %d\n", dest, achaNext(dest));
+  sleep(1);
+  send_next(achaNext(dest));
+
+  op=-1;
+  msg_env_control++;
+
+}
+
+int achaNext(int destiny){
+  if(destiny > ID){
+
+    for (int i = 0; i < NROTEADORES; i++) {
+          if (tabelaroteamento[i][destiny] + tabelaroteamento[ID][i] == vetordistancia[destiny] && i!=ID && i!=destiny) {
+            return i;
+          }
+        }
+  }else if(ID > destiny){
+    for (int i = NROTEADORES; i != 0; i--) {
+          if (tabelaroteamento[i][destiny] + tabelaroteamento[ID][i] == vetordistancia[destiny] && i!=ID && i!=destiny) {
+            return i;
+          }
+        }
+  }
+
+  return destiny;
+}
+
+void send_next(int next){
+
+  pthread_mutex_lock(&mutex);
+
+  printf("Enviando mensagem para %d\n", next);
+  sleep(2);
+
+  si_other.sin_port= htons(roteadores[next].porta);
+  if(inet_aton(roteadores[next].ip , &si_other.sin_addr) == 0){//se tiver erro no endereço
+    die("Falha ao obter ip do destino\n");
+  }else{
+    if(sendto(sock, &caixa_env[msg_env_control],sizeof(caixa_env[msg_env_control]), 0,(struct sockaddr*) &si_other, sizeof(si_other)) ==-1){
+      die("Falha no envio da mensagem\n");//se tiver erro na mensagem
+    }else{
+      printf("Roteador %d enviando Mensagem para %d\n", ID, roteadores[next].id);
+    }
+  }
+
+  pthread_mutex_unlock(&mutex);
+
+}
+
+void criatabela(){
+
+  int i,j;
+
+  for(i=0; i<NROTEADORES; i++){//prenche a tabela com infinito
+		for(j=0; j<NROTEADORES; j++){
+		    tabelaroteamento[i][j]=1000;
+		}
+	}
+  for(i=0, j=0; i< NROTEADORES; i++, j++){//zera a diagonal principal
+    tabelaroteamento[i][j]=0;//custos pra vc mesmo é zero
+  } 
+
+  copia_vetor_para_tabela(ID, vetordistancia);//chama essa função para copiar o vd para a tabela
+
+}
+
+void copia_vetor_para_tabela(int id, int vetdist[NROTEADORES]){//passa o vetor distancia do roteador para a tabela de roteamento
+
+  int i,j;
+
+  for(i=0; i<NROTEADORES; i++){
+    tabelaroteamento[id][i]= vetdist[i];
+  }
+
+}
+
+void atualiza_vetor_vizinhos(){
+
+  int i;
+
+  for(i=0; i<NROTEADORES; i++){
+    if(vetordistancia[i]>=0 && vetordistancia[i]<100){
+      vetorvizinhos[i]=i;
+    }
+  }
+
+  vetorvizinhos[ID]=0;
+}
+
+void imprime_tabela_roteamento(){
+
+  int i,j;
+
+  //debug
+  for(j=0; j<NROTEADORES; j++){
+    for(i=0; i<NROTEADORES; i++){
+      printf("%d\t", tabelaroteamento[j][i]);
+    }
+    printf("\n");
+  }
+
+  sleep(10);
+  op=-1;
+
+}
+
+void criavetor(){//cria a primeira vez o vetor distancia do roteador
+
+  int x,y,i,custo;
+
+  //////////////////////////////////////////
+  //inicia os vetores dis e de vetorvizinhos
+
+  for(i=0; i<NROTEADORES; i++){//inicializa os vetores com -1
+    vetorvizinhos[i]=-1;
+    vetordistancia[i]=1000;
+  }
+  //vetorvizinhos[ID]=0;//seta os valores para mim mesmo como zero
+  vetordistancia[ID]=0;
+
+//atribui os custos dos vizinhos par ao vet distancia
+
+  FILE *p = fopen("enlaces.config", "r");//abre o arquivo enlaces em modo leitura
+	if(!p){
+		die("Erro ao abrir arquivo enlaces.config\n");
+	}
+  while(fscanf(p, "%d %d %d\n", &x, &y, &custo) != EOF){//enquanto a leitura do arquivo não chega ao fim
+		if(x==ID){
+      vetordistancia[y]=custo;
+    }
+    if(y==ID){
+      vetordistancia[x]=custo;
+    }
+	}
+	fclose(p);
+
 }
 
 void criar_roteador(int ID){//insrancia roteador, cria socket e faz bind com a porta
@@ -85,7 +317,115 @@ void * recebe(void * nada){//recebe mensagens e retransmite se necessário
 			printf("Erro ao receber mensagem\n");
 		}
 
+    if(temp_msg.tipo==0){//zero é tipo mensagem
+
+      if(temp_msg.destino==ID){//ve se é pra mim
+        printf("Mensagem recebida de %d\n", temp_msg.origem);
+        caixa_msg[msg_control]=temp_msg;//salva a mensagem
+        msg_control++;
+        //confirmar aqui
+        ///////////////////////
+        printf("Enviando confirmação para %d!\n", temp_msg.origem);
+        caixa_env[msg_env_control]=temp_msg;
+        caixa_env[msg_env_control].tipo=2;
+        caixa_env[msg_env_control].origem=ID;
+        caixa_env[msg_env_control].destino=temp_msg.origem;
+        send_next(achaNext(caixa_env[msg_env_control].destino));
+        msg_env_control++;
+
+        //////////////////////
+      }else{//reenviar para o proximo sendnext
+        
+        caixa_env[msg_env_control]=temp_msg;
+        send_next(achaNext(caixa_env[msg_env_control].destino));
+        msg_env_control++;
+
+      }
+
+    }else if(temp_msg.tipo==1){//tipo atualização de rota
+      
+      copia_vetor_para_tabela(temp_msg.origem, temp_msg.vetordist);//passa o vetor para a tabela de roteamento
+      atualiza_vetor();
+
+    }else if(temp_msg.tipo==2){//tipo confirmação
+
+      if(temp_msg.destino==ID){//ve se a confirmação é pra mim
+
+        printf("Confirmado. %d recebeu a sua mensagem!\n", temp_msg.origem);
+      }else{//senao repassa a confirmação
+        sleep(2);
+        printf("Enviando confirmação!\n");
+        caixa_env[msg_env_control]=temp_msg;
+        send_next(achaNext(caixa_env[msg_env_control].destino));
+        msg_env_control++;
+
+
+      }
+
+      
+
+    }
 	}
+}
 
+void atualiza_vetor() {
 
+  pthread_mutex_lock(&mutex);
+  int custo, i, j;
+  for (i = 0; i < NROTEADORES; i++) {
+      custo = vetordistancia[i];
+      if(i != ID){
+        for (j = 0; j < NROTEADORES; j++) {
+            if (j != ID && (custo > (tabelaroteamento[j][i] + tabelaroteamento[ID][j]))) {
+                custo = (tabelaroteamento[j][i] + tabelaroteamento[ID][j]);
+            }
+        }
+        vetordistancia[i] = custo;
+    }
+  }
+
+  copia_vetor_para_tabela(ID, vetordistancia);
+  pthread_mutex_unlock(&mutex);
+
+}
+
+void * envia_vetor(void * nada){
+
+  int i;
+
+  while(1){
+
+    sleep(10);
+
+    pthread_mutex_lock(&mutex);
+
+    temp_rota.origem=ID;
+    temp_rota.tipo=1;//tippo att de rota
+    
+    for(i=0; i<NROTEADORES; i++){
+      temp_rota.vetordist[i]=vetordistancia[i];
+    }
+
+    for(i=0; i<NROTEADORES; i++){
+      if(vetorvizinhos[i]>=0){//escolhe um vizinho válido
+        send_next_vetor(i);
+        
+      }
+    }
+
+    pthread_mutex_unlock(&mutex);
+  }
+
+}
+
+void send_next_vetor(int next){//manda o vetor de distancias para os vizinhos
+
+  si_other.sin_port= htons(roteadores[next].porta);
+  if(inet_aton(roteadores[next].ip , &si_other.sin_addr) == 0){//se tiver erro no endereço
+    die("Falha ao obter ip do destino\n");
+  }else{
+    if(sendto(sock, &temp_rota,sizeof(temp_rota), 0,(struct sockaddr*) &si_other, sizeof(si_other)) ==-1){
+      die("Falha no envio da mensagem\n");//se tiver erro na mensagem
+    }
+  }
 }
--- T2 - Roteamento com UDP e Bellman ford - Distribuído/roteador.h
+++ T2 - Roteamento com UDP e Bellman ford - Distribuído/roteador.h
@@ -16,8 +16,9 @@
 #include <pthread.h>
 
 
-#define NROTEADORES 6 // Quantidade de Roteadores
+#define NROTEADORES 6 // Quantidade de Roteadores BUFFER
 #define MENSAGEM_SIZE 100 // Quantidade de caracteres em uma mensagem
+
 
 
 typedef struct{//estrutura que cria um roteador para a rede
@@ -26,10 +27,22 @@ typedef struct{//estrutura que cria um roteador para a rede
 }roteador;
 
 typedef struct{//estrutura que cria uma mensagem enviada/recebida por um roteador
-	int origem, destino, num;
+	int origem, destino, tipo;//orig, dest, flag de tipo de msg
 	char msg[MENSAGEM_SIZE];
+	int vetordist[NROTEADORES];
 }mensagem;
 
 void die(char *s);//imprime erros
 void criar_roteador(int ID);//cria roteador
 void * recebe(void * nada);
+void * envia_vetor(void * nada);
+void criavetor();
+void criatabela();
+void copia_vetor_para_tabela(int id, int vetdist[NROTEADORES]);
+void envia();
+void send_next(int next);
+void atualiza_vetor_vizinhos();
+void imprime_tabela_roteamento();
+void send_next_vetor(int next);
+void atualiza_vetor();
+int achaNext(int destiny);
